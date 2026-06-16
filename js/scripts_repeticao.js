4cda4722a07e4510f74f168aa893d051eb3abf89
Eloisa Zifirino <eloisa.zeferino@aluno.senai.br>
2026-06-16 16:51:22 -0300

    adicionado os contadores da estrutura de repetição

--- js/scripts_repeticao.js
+++ js/scripts_repeticao.js
@@ -2,5 +2,24 @@
 const divFor = document.querySelector('#div-for')
 
 for( i = 0; i < 10; i++){
-    console.log(i)
+    divFor.innerHTML += `${i} - Amor<br>`
 }
+
+//CONTADOR/ACUMULADOR
+const inputNum = document.querySelector('#num')
+const brnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+brnNum.addEventListener('click', (evt)=>{
+    let numDigitado = Number(inputNum.value)
+
+    cont++
+    acum += numDigitado
+
+    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br> A SOMA DOS NÚMERO É: ${acum}`
+
+    inputNum.value = ''
+})
