d8322b75f5506fd7b59fb4200189931eba1ff47f
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-17 14:37:56 -0300

    Adicionado os scripts das divs btn-frase e repeticao

--- js/scripts_repeticao.js
+++ js/scripts_repeticao.js
@@ -2,18 +2,18 @@
 const divFor = document.querySelector('#div-for')
 
 for( i = 0; i < 10; i++){
-    divFor.innerHTML += `${i} - Amor<br>`
+    divFor.innerHTML += `${i} - Amor <br>`
 }
 
 //CONTADOR/ACUMULADOR
 const inputNum = document.querySelector('#num')
-const brnNum = document.querySelector('#btn-num')
+const btnNum = document.querySelector('#btn-num')
 const divContAcum = document.querySelector('#div-cont-acum')
 
 let cont = 0
 let acum = 0.0
 
-brnNum.addEventListener('click', (evt)=>{
+btnNum.addEventListener('click', (evt)=>{
     let numDigitado = Number(inputNum.value)
 
     cont++
@@ -23,3 +23,20 @@ brnNum.addEventListener('click', (evt)=>{
 
     inputNum.value = ''
 })
+
+//CONTROLADOR FOR
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('#div-frase')
+
+btnFrase.addEventListener('click', (evt)=>{
+    let numRepeti = Number(inputNumRepeticao.value)
+    let frase = inputFrase.value
+
+    for(i = 0; i < numRepeti; i++){
+        divFrase.innerHTML += `${i} - ${frase} <br>`
+    }
+})
+
+//FOR IN
